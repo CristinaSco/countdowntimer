2d12a4a92bd5182f25fdd435a67b36541e536d76
Cristina Scognamiglio <77617593+CristinaSco@users.noreply.github.com>
2021-06-22 16:40:28 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date("Dec 25, 2021 15:37:25").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("date").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("date").innerHTML = "EXPIRED";
+  }
+}, 1000);
